4cbc3da651aa9fadae752745ce9fe541d5cf571f
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-26 19:20:37 +0000

    build cli

--- longboards/lib/longboards/boards.rb
+++ longboards/lib/longboards/boards.rb
@@ -1,2 +1,18 @@
-class Boards
+class Board
+  #what attributes will a board have
+  attr_accessor :name, :url, :price, :description
+
+  @@all = []
+
+  def initialize(name, url)
+    @name = name
+    @url = url
+    @@all << self
+  end
+
+
+
+  def self.all
+    @@all
+  end
 end
