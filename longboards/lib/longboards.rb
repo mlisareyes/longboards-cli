cb0f9ef37cc0e589b7ab0aff00375ed4251b928c
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-26 03:22:14 +0000

    two

--- longboards/lib/longboards.rb
+++ longboards/lib/longboards.rb
@@ -1,5 +1,12 @@
-require "longboards/version"
+### REQUIRED GEMS ###
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
 
+### NAMESPACE MODULE ###
 module Longboards
   # Your code goes here...
 end
+
+### REQUIRED FILES ###
+require "longboards/version"
