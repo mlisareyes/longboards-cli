0b0f697884ae02269359337a04f75a6710d0b24a
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-26 19:08:12 +0000

    build cli

--- longboards/lib/longboards/cli.rb
+++ longboards/lib/longboards/cli.rb
@@ -12,9 +12,17 @@ class Longboards::CLI
     if input == "list"
       list_boards
     elsif input == "exit"
-      puts "Thank you for visiting Warehouse Skatboards! Please visit again!"
+      puts "Thank you for visiting Warehouse Skateboards! Please visit again!"
     else
       invalid
       main_menu
+    end
+  end
+
+  def list_boards
+  end
+
+  def invalid
+    puts "I'm sorry, I do not understand your response."
   end
 end
