649c131f3288f487c0edd41b741712f2c8860443
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-26 17:03:40 +0000

    eight

--- longboards/lib/longboards/scraper.rb
+++ longboards/lib/longboards/scraper.rb
@@ -5,7 +5,13 @@ class Longboards::Scraper
   def self.scrape_boards
     doc = Nokogiri::HTML(open("https://www.warehouseskateboards.com/longboard-completes"))
     doc.css(".arrayProduct").map do |longboard|
-      binding.pry
+      name = longboard.css(".arrayNew").text
+      url = longboard.css("a")[0]("href")
+      longboard = Longboard.new(name, url)
     end
+  end
+
+  def self.scrape_product_page
+    doc = Nokogiri::HTML(open(BASE_URL + longboard.url))
   end
 end
